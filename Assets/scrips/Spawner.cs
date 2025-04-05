58df0845896c5bce57f01168d440db903d8f4eb0
qkrgml59 <qkrgml59@gmail.com>
2025-04-05 17:14:42 +0900

    [fead] 생성 및 화용

---
+++ Assets/scrips/Spawner.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject coinPrefabs;
+
+    [Header("스폰 타이밍 설정")]
+    public float minSpawnlnterval = 0.5f;
+    public float maxSpawnlnterval = 2.0f;
+
+    public float timer = 0.0f;
+    public float nextSpawnTime;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNextSpawnTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= nextSpawnTime)
+        {
+            SpawnObject();
+            timer = 0.0f;
+            SetNextSpawnTime();
+
+        }
+
+    }
+    void SpawnObject()
+    {
+        Transform spawnTransform = transform;
+        Instantiate(coinPrefabs, spawnTransform.position, spawnTransform.rotation);
+
+    }
+    void SetNextSpawnTime()
+    {
+        nextSpawnTime = Random.Range(minSpawnlnterval, maxSpawnlnterval);
+    }
+}
